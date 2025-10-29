b74c432b92e7c1efa6a097310719dd2366c4f8a4
ridwan <ridwanullahgbolagade@gmail.com>
2025-10-29 13:58:02 +0100

    add

--- server/transproter.js
+++ server/transproter.js
@@ -1,10 +1,13 @@
 const nodemailer = require("nodemailer")
 
 const transporter = nodemailer.createTransport({
-  service: "gmail", 
+  host: "smtp.gmail.com",
+  port: 587,
+  service: "Gmail",
+  secure: false,
   auth: {
     user: process.env.appEmail,
-    pass: process.env.appPassword, 
+    pass: process.env.appPassword,
   },
 });
 
