8d3644dcc3a86451c64f24e51b1f24337a274ebe
ridwan <ridwanullahgbolagade@gmail.com>
2025-10-30 18:56:32 +0100

    added

--- controllers/uploadImage.js
+++ controllers/uploadImage.js
@@ -1,19 +1,17 @@
-const multer = require("multer");
+const multer = require("multer")
+const { CloudinaryStorage } = require("multer-storage-cloudinary")
+const cloudinary = require("./cloudinary")
 
-const CloudinaryStorage = require("multer-storage-cloudinary")
-const cloudinary = require("cloudinary")
 
-// Configure the storage engine for Multer
+
 const newStorage = new CloudinaryStorage({
     cloudinary: cloudinary,
     params: {
-        folder: "star_images", // Folder in your Cloudinary account
-        allowedFormats: ["jpeg", "png", "jpg"],
+        folder: "star_images",
+        allowFormats: ["jpeg", "png", "jpg"],
         transformation: [{ width: 500, height: 500, crop: "limit" }],
-    }
-});
+      }
+})
 
-// Create the Multer instance
 const uploadImage = multer({ storage: newStorage });
-
 module.exports = uploadImage;
